fb5066914e0ec994f5cbda3c8e193e6d0ffd6f96
Chandan Kumar Behera <118505620+chandankumar1425@users.noreply.github.com>
2023-07-03 11:59:36 +0530

    github

---
+++ mock4/script/login.js
@@ -0,0 +1,37 @@
+document.querySelector("#myLogin_form").addEventListener("submit",Formsubmit_Login)
+
+function Formsubmit_Login(e){
+    e.preventDefault()
+    console.log("form")
+
+    let email = document.querySelector("#EM").value
+    let password = document.querySelector("#PS").value
+
+    // console.log(name,age,place,batch,profession)
+
+    let obj = {
+     email,password
+    }
+
+   RegisterUser(obj)
+}
+
+async function RegisterUser(data){
+    console.log(data)
+    let response =await fetch("https://reqres.in/api/login",{
+        method:"POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify({
+       ...data
+        })
+
+    })
+    let token = await response.json()
+    if(token){
+   localStorage.setItem("token",JSON.stringify(token.token))
+   alert("Login successful")
+   window.location.href = "./data.html"
+    }else{
+        alert("something went wrong try again")
+    }
+}
